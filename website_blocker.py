41a743269ebaca843bfa6197915c914246c71512
robertmoore40 <robertcmoore92@gmail.com>
2020-06-09 21:30:28 -0400

    start

---
+++ website_blocker.py
@@ -0,0 +1,8 @@
+#website blocking
+hosts_path="C:\Windows\System32\drivers\etc\hosts"
+
+redirect="127.0.0.1"
+
+website_list=['facebook.com', 'www.facebook.com', 'instagram.com', 'www.instagram.com']
+
+
