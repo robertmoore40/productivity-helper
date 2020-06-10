eae008da3d3895e22db03371b3ddb289a19b183b
robertmoore40 <robertcmoore92@gmail.com>
2020-06-09 22:00:00 -0400

    hours picked

--- website_blocker.py
+++ website_blocker.py
@@ -1,8 +1,19 @@
 #website blocking
+import time
+
+from datetime import datetime as dt
+
 hosts_path="C:\Windows\System32\drivers\etc\hosts"
 
 redirect="127.0.0.1"
 
-website_list=['facebook.com', 'www.facebook.com', 'instagram.com', 'www.instagram.com']
+website_list=['facebook.com', 'www.facebook.com', 'instagram.com', 'www.instagram.com', 'www.twitter.com', 'twitter.com']
 
-
+while True:
+    if dt(dt.now().year, dt.now().month, dt.now().day, 8) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 16):
+        print(dt.now())
+        print("Currently in work mode, social media is blocked")
+        time.sleep(5)
+    else:
+        print("Free time")
+        time.sleep(5)
